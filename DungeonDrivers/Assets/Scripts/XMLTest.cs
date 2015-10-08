10a45e8fc4a9f309afc504eadc27403eed0f0e54
mrmaarek <markdeheij@hotmail.com>
2015-10-08 11:32:12 +0200

    Cards are loaded from XML and the card data is also filled from the XML sheet.

--- DungeonDrivers/Assets/Scripts/XMLTest.cs
+++ DungeonDrivers/Assets/Scripts/XMLTest.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System;
-using System.Xml.Serialization;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -12,36 +11,76 @@ public class XMLTest : MonoBehaviour
     XmlDocument xmlDoc = new XmlDocument();
     string myPath;
 
-    public GameObject CardTest;
-    public Text CardTekst;
+    [SerializeField]
+    private GameObject myHand;
+
+
+    public GameObject Card;
+    [SerializeField]
+    private GameObject cardName;
+    [SerializeField]
+    private GameObject cardDescription;
+
+    private Text CardTekst;
     //public Prefab testpref;
 
     void Awake()
     {
         myPath = Application.dataPath + "/XML/cards.xml";
-        Debug.Log(myPath);
+        //Debug.Log(myPath);
         xmlDoc.Load(myPath);
+ 
 
 
-
-        CardTekst = CardTest.GetComponent<Text>();
+        CardTekst = Card.GetComponent<Text>();
     }
     // Use this for initialization
     void Start ()
-    {
-        CardTekst.text = "MWAH";
-        //Debug.Log(testText);
-        //
-        Debug.Log(xmlDoc);
+    {        
+        //ca
+        XmlNodeList cardNames = xmlDoc.GetElementsByTagName("name");
+        int totalCards = cardNames.Count;
 
-        XmlNodeList cardName = xmlDoc.GetElementsByTagName("name");
+        //Debug.Log(totalCards);
+
         XmlNodeList cardDescription = xmlDoc.GetElementsByTagName("description");
 
-        for (int i = 0; i < cardName.Count; i++)
+        for (int cardID = 0; cardID < totalCards; cardID++)
         {
-            Instantiate(CardTest);
+            //CreateCard(cardNames[cardID].InnerXml, cardID);
+           
+            /*
+            Card.transform.SetParent(myHand.transform);
+            Card.name = cardNames[cardID].InnerXml;
+            Card.GetComponent<Text>().text = cardNames[cardID].InnerXml;
+            Card.SetActive(true);
+            */
+            //cardName = cardNames[cardID].InnerXml;
+            // Debug.Log(cardNames[cardID] + " " + cardNames[cardID].InnerXml);
+            //Debug.Log(cardNames[cardID].InnerXml);
 
 
+            GameObject newCard = Instantiate(Card) as GameObject;
+            newCard.transform.SetParent(myHand.transform);
+            newCard.name = cardNames[cardID].InnerXml;
+
+
+            newCard.transform.GetChild(1).GetComponent<Text>().text = cardNames[cardID].InnerXml;
+            newCard.transform.GetChild(2).GetComponent<Text>().text = cardDescription[cardID].InnerXml;
+            //cardName = newCard.transform.GetChild(1);
+            //newCard.
+            //cardName.GetComponent<Text>().text = cardNames[cardID].InnerXml;
+            //newCard.GetComponentInChildren<Text>().text = cardName;
+
+            //newCard.GetComponentInChildren<Text>().text = cardNames[cardID].InnerXml;
+            //newCard.GetComponent<Text>().text = cardNames[cardID].InnerXml;
+
+
+            newCard.SetActive(true);
+            
+            newCard.transform.localScale = new Vector3(1,1,1);
+           // newCard.transform.position = new Vector3(0, 0, 0);
+            newCard.transform.localPosition = new Vector3(0, 0, 0);
             //testText.text = cardName[i].InnerXml;
 
             /*
@@ -52,7 +91,7 @@ public class XMLTest : MonoBehaviour
 
         //xmlDoc.Close();
     }
-	
+   
 	// Update is called once per frame
 	void Update () {
 	
