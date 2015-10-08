c78c576b30af6c68431cb34be27ad9ac654580e4
mrmaarek <markdeheij@hotmail.com>
2015-10-08 12:50:19 +0200

    Deleted more unnecessary code

--- DungeonDrivers/Assets/Scripts/XMLTest.cs
+++ DungeonDrivers/Assets/Scripts/XMLTest.cs
@@ -26,55 +26,29 @@ public class XMLTest : MonoBehaviour
 
     void Awake()
     {
+        //Load the XML document with his path
         myPath = Application.dataPath + "/XML/cards.xml";
-        //Debug.Log(myPath);
         xmlDoc.Load(myPath);
- 
-
 
         CardTekst = Card.GetComponent<Text>();
     }
     // Use this for initialization
     void Start ()
     {        
-        //ca
+        
         XmlNodeList cardNames = xmlDoc.GetElementsByTagName("name");
         int totalCards = cardNames.Count;
-
-        //Debug.Log(totalCards);
 
         XmlNodeList cardDescription = xmlDoc.GetElementsByTagName("description");
 
         for (int cardID = 0; cardID < totalCards; cardID++)
         {
-            //CreateCard(cardNames[cardID].InnerXml, cardID);
-           
-            /*
-            Card.transform.SetParent(myHand.transform);
-            Card.name = cardNames[cardID].InnerXml;
-            Card.GetComponent<Text>().text = cardNames[cardID].InnerXml;
-            Card.SetActive(true);
-            */
-            //cardName = cardNames[cardID].InnerXml;
-            // Debug.Log(cardNames[cardID] + " " + cardNames[cardID].InnerXml);
-            //Debug.Log(cardNames[cardID].InnerXml);
-
-
             GameObject newCard = Instantiate(Card) as GameObject;
             newCard.transform.SetParent(myHand.transform);
             newCard.name = cardNames[cardID].InnerXml;
 
-
             newCard.transform.GetChild(1).GetComponent<Text>().text = cardNames[cardID].InnerXml;
             newCard.transform.GetChild(2).GetComponent<Text>().text = cardDescription[cardID].InnerXml;
-            //cardName = newCard.transform.GetChild(1);
-            //newCard.
-            //cardName.GetComponent<Text>().text = cardNames[cardID].InnerXml;
-            //newCard.GetComponentInChildren<Text>().text = cardName;
-
-            //newCard.GetComponentInChildren<Text>().text = cardNames[cardID].InnerXml;
-            //newCard.GetComponent<Text>().text = cardNames[cardID].InnerXml;
-
 
             newCard.SetActive(true);
             
