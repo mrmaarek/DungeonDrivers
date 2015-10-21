f228646f5bcaca63cd192935b856494500a9bdb5
mrmaarek <markdeheij@hotmail.com>
2015-10-21 11:52:39 +0200

    Reorganized the folder structure
    Added klaas grid
    Added a new font for testing the text on the fonts.
    Now you can choose which class you want to play and the cards of the
    chosen class will load.

---
+++ DungeonDrivers/Assets/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public Grid_Spawner_Script Grid_Spawner_Script;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Grid_Spawner_Script.SetupGrid();
+	}
+	
+	
+}
---
+++ DungeonDrivers/Assets/Klaas/Scripts/Grid_Block_Script.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Grid_Block_Script : MonoBehaviour
+{
+	public GameObject CurrentPlayer;
+	//public List<bool> allPlayersInside = new List<bool>();
+	public bool[] playersInSide = new bool[4];
+	public SpriteRenderer SpriteRenderer;
+	public bool isActive, isSelected, isMovePos, isCardPos;
+	//public GameObject Top, Right, Bot, Left;
+	//public GameObject TopLeft, TopRight, BotLeft, BotRight;
+	//public List<GameObject> surroundingGridBlocks = new List<GameObject>();
+
+	public int xLane, yLane;
+
+	public Sprite active, unActive, ActiveSelected, UnactiveSelected, MovePos, CardPos;
+
+	void FixedUpdate()
+	{
+		if(isCardPos)
+		{
+			SpriteRenderer.sprite = CardPos;
+		}
+		else if(isMovePos)
+		{
+			SpriteRenderer.sprite = MovePos;
+		}
+		else if(isActive)
+		{
+			if(isSelected)
+			{
+				SpriteRenderer.sprite = ActiveSelected;
+			}
+			else
+			{
+				SpriteRenderer.sprite = active;
+			}
+		}
+		else
+		{
+			if(isSelected)
+			{
+				SpriteRenderer.sprite = UnactiveSelected;
+			}
+			else
+			{
+				SpriteRenderer.sprite = unActive;
+			}
+		}
+	}
+
+}
