02dd0e0a5614f238d3df5a0387b7698b012e0c0b
mrmaarek <markdeheij@hotmail.com>
2015-10-29 16:01:22 +0100

    Mark update, now you can choose a class
    Choose class before ready

---
+++ DungeonDrivers/Assets/MARK/ClassSelector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClassSelector : MonoBehaviour
+{
+
+    public enum Classes
+    {
+        None_Selected,
+        Sandmage,
+        Warrior
+    }
+
+    public Classes playerClass;
+
+    void Start()
+    {
+        this.playerClass = Classes.None_Selected;
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ClassSelect(int pClass)
+    {
+        this.playerClass = (Classes)pClass;
+    }
+}
