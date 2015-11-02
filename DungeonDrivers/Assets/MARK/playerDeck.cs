698c02cdb6f80fa1f99dfc1275a6595aabe2f008
mrmaarek <markdeheij@hotmail.com>
2015-11-02 12:39:37 +0100

    Loading the correct deck on your choice
    Still testing it.

---
+++ DungeonDrivers/Assets/MARK/playerDeck.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+using System.Collections;
+
+
+//myDeck = Resources.LoadAll("Resources/Cards/Warrior/", typeof(GameObject));
+
+ 
+public class playerDeck : MonoBehaviour
+{
+    string deckLocation;
+    public GameObject[] myDeck;
+
+    [SerializeField]
+    private GameObject mySelectedClass;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Check which Class is selected.
+        // And load the corresponding cards.
+        if (mySelectedClass.GetComponent<ClassSelector>().playerClass == ClassSelector.Classes.Sandmage)
+        {
+            deckLocation = "Cards/SandMage";
+        }
+        if (mySelectedClass.GetComponent<ClassSelector>().playerClass == ClassSelector.Classes.Warrior)
+        {
+            deckLocation = "Cards/Warior";
+        }
+
+        generateDeck();
+        spawnDeck();
+       
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	
+	}
+
+    void generateDeck()
+    {
+        myDeck = Resources.LoadAll<GameObject>("Cards/Warior");
+        //myDeck = Resources.LoadAll<GameObject>(deckLocation);
+        Debug.Log(myDeck);
+    }
+
+    void spawnDeck()
+    {
+        foreach (GameObject card in myDeck)
+        {
+            //Instantiate(card) as GameObject;
+
+            Instantiate(card, new Vector3(0,0,0), Quaternion.identity);
+        }
+    }
+}
