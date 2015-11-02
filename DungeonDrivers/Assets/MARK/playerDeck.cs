0edf5a25c2f6e1d5683a3c889df6d180c6e0ce27
mrmaarek <markdeheij@hotmail.com>
2015-11-02 15:38:58 +0100

    Added a few things
    - New card prefabs for testing issues.
    - Made an Array and a List of the deck.
    - DrawHand draws now 3 cards in your hand.

--- DungeonDrivers/Assets/MARK/playerDeck.cs
+++ DungeonDrivers/Assets/MARK/playerDeck.cs
@@ -10,7 +10,10 @@ using System.Collections;
 public class playerDeck : MonoBehaviour
 {
     string deckLocation;
-    public GameObject[] myDeck;
+    
+
+    
+    
 
     [SerializeField]
     private GameObject mySelectedClass;
@@ -18,8 +21,12 @@ public class playerDeck : MonoBehaviour
     [SerializeField]
     private Transform myDeckPanel;
 
-   
 
+    public GameObject[] myDeck;                                         //Array of Deck
+    [SerializeField]
+    private List<GameObject> myTempDeck = new List<GameObject>();       //List of Deck
+    [SerializeField]
+    private List<GameObject> myTempHand = new List<GameObject>();
 
     // Use this for initialization
     void Start ()
@@ -38,26 +45,33 @@ public class playerDeck : MonoBehaviour
         generateDeck();
         spawnDeck();
         shuffleDeck();
+        drawHand();
 
     }
 
     // Update is called once per frame
     void Update ()
     {
-        
+        //shuffleDeck();
     }
 
     void generateDeck()
     {
+        // The Deck is loaded in a array.
         myDeck = Resources.LoadAll<GameObject>("Cards/Warior");
-        //myDeck = Resources.LoadAll<GameObject>(deckLocation);
-        Debug.Log(myDeck);
+      
+        // The Deck is loaded to a list instead of an array.
+        for(int i = 0; i < myDeck.Length; i++)
+        {
+            // Add every element of the array to the list.
+            myTempDeck.Add(myDeck[i]);
+        }
     }
 
     void spawnDeck()
     {
        
-        foreach (GameObject card in myDeck)
+        foreach (GameObject card in myTempDeck)
         {
             GameObject newCard = Instantiate(card) as GameObject;
             newCard.transform.SetParent(myDeckPanel);
@@ -65,25 +79,36 @@ public class playerDeck : MonoBehaviour
             newCard.transform.localPosition = new Vector3(0, 0, 0);
             newCard.transform.Rotate(new Vector3(90, 180, 0));
             newCard.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-
             
         }
     }
 
     void shuffleDeck()
     {
-        Debug.Log("There currently are... " + myDeck.Length + " cards in your deck." );
+        Debug.Log("There currently are... " + myTempDeck.Count + " cards in your deck." );
 
-        for (int i = 0; i < myDeck.Length; i++)
+        for (int i = 0; i < myTempDeck.Count; i++)
         {
             // For each card make a temporary copy.
-            GameObject tempCard = myDeck[i];
+            GameObject tempCard = myTempDeck[i];
             int randomIndex = UnityEngine.Random.Range(0, myDeck.Length);
-            myDeck[i] = myDeck[randomIndex];
-            myDeck[randomIndex] = tempCard;
+            myTempDeck[i] = myTempDeck[randomIndex];
+            myTempDeck[randomIndex] = tempCard;
+
+            Debug.Log(myTempDeck[i]);
+            Debug.Log(myTempDeck[randomIndex]);
 
             //Destroy(tempCard);
         }
 
+    }
+
+    void drawHand()
+    {
+        int cardsToDraw = 3;
+        for (int i = 0; i < cardsToDraw; i++)
+        {
+            myTempHand.Add(myTempDeck[i]);
+        }
     }
 }
