a2ae41396484e53e088ab5902b98802721a3b80e
mrmaarek <markdeheij@hotmail.com>
2015-11-03 10:34:35 +0100

    Ready to start is RED when you havent selected
    a class

--- DungeonDrivers/Assets/MARK/ClassSelector.cs
+++ DungeonDrivers/Assets/MARK/ClassSelector.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ClassSelector : MonoBehaviour
 {
+    public Button readyToStart;
+
 
     public enum Classes
     {
@@ -16,10 +19,21 @@ public class ClassSelector : MonoBehaviour
     void Start()
     {
         this.playerClass = Classes.None_Selected;
+        readyToStart.enabled = false;
+        readyToStart.image.color = Color.red;
     }
 
     void Update()
     {
+        // WHEN PLAYER CLASS IS NOT NONE SELECTED.
+        if (playerClass != Classes.None_Selected)
+        {
+            // MAKE THE READY BUTTON ACTIVE.
+            readyToStart.image.color = Color.white;
+            readyToStart.enabled = true;
+        }
+
+        // When i don't press any of the class 'play' buttons i need to reset the playerClass to NoneSelected.
 
     }
 
